517f18678518b4b32ce56b64e67d276db664bfbd
maximilianou <maximilianou@gmail.com>
2018-11-14 14:30:09 -0300

    initial, from the begining

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: 'postrgresql://educacion:educacion@localhost:5432/instrument_db',
+})
+
+class DB {
+
+  static async view(){
+    const { rows } = await pool.query('SELECT * FROM users WHERE u_id = $1', [1])
+    return rows[0];
+  }
+  static async count(){
+    const many_ones = await pool.query(' SELECT COUNT(*) FROM users ')
+    return many_ones;
+  }
+  static async viewAll(){
+    const { rows } = await pool.query('SELECT * FROM users ');
+    return rows;
+  }
+  static async insert( parameter ){
+    const { rows } = await pool.query(' INSERT INTO users ( u_name, u_email )VALUES( $1, $2 ) ', parameter )
+    return JSON.stringify(rows); 
+  }
+  static async update( parameter ){
+    const { rows } = await pool.query(' UPDATE users set u_email = $2 WHERE u_name =  $1 ', parameter )
+    return JSON.stringify(rows); 
+  }
+  static async remove( parameter ){
+    const { rows } = await pool.query(' DELETE FROM users WHERE u_name = $1 ', parameter );
+    return JSON.stringify(rows);
+  }
+  static async end(){
+    pool.end();
+  }
+}
+
+module.exports = DB;
---
+++ test/users.js
@@ -0,0 +1,51 @@
+const db = require('../server');
+const should = require('should');
+
+describe("Database access : CRUD", () => {
+
+  const user = ['Batman','bat@gmail.com']; 
+
+  describe("Number! to Number!", ()=> {
+    it("should check number by number", ()=>{
+       let one = 1;
+       let two = 2;
+       let once = 1;
+       one.should.eql.once;
+       one.should.not.eql.two;
+    });
+  });
+
+  describe("SQL Create: ", ()=>{
+    it("should insert a new element ", ()=>{
+      const how_many = db.count();
+      db.insert( user );
+      db.count().should.not.eql.how_many;
+    });
+  });
+  describe("SQL Retrieve: ", ()=>{
+    it("should return the element ", async ()=>{
+      const how_many = await db.count();
+      const all_of_them = await db.viewAll( user );
+      all_of_them.length.should.not.eql.how_many;
+    });
+  });
+  describe("SQL Update: ", ()=>{
+    it("should update the element ", ()=>{
+      const how_many = db.count();
+      user[1] = 'robin@yahoo.com';
+      db.update( user );
+      db.count().should.eql.how_many;
+    });
+  });
+  describe("SQL Delete: ", ()=>{
+    it("should remove the element ", ()=>{
+      const how_many = db.count();
+      db.remove( [ user[0] ] );
+      db.count().should.not.eql.how_many;
+    });
+  });
+
+  after("Database Pooling Connections Closing..", ()=>{
+    db.end();
+  });
+});
