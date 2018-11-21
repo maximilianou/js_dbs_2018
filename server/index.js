db0e1911fbfa05baddde49665fc9d856f8e205d4
maximilianou <maximilianou@gmail.com>
2018-11-21 13:45:58 -0300

    connection pool, pooling.

--- server/index.js
+++ server/index.js
@@ -7,41 +7,84 @@ const pool = new Pool({
 class DB {
 
   static async view(){
-    const { rows } = await pool.query('SELECT * FROM users WHERE u_id = $1', [1])
-    return rows[0];
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query('SELECT * FROM users WHERE u_id = $1', [1]);
+      return rows[0];
+    }finally{
+      await client.release();
+    }
   }
   static async count(){
-    const many_ones = await pool.query(' SELECT COUNT(*) FROM users ')
-    return many_ones.rows[0].count;
+    let client = await pool.connect(); 
+    try{
+      const many_ones = await client.query(' SELECT COUNT(*) FROM users ')
+      return many_ones.rows[0].count;
+    }finally{
+      await client.release();
+    }
   }
   static async viewAll(){
-    const { rows } = await pool.query('SELECT * FROM users ');
-    return rows;
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query('SELECT * FROM users ');
+      return rows;
+    }finally{
+      await client.release();
+    }
+  }
+  
+  static async viewByName( param ){
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query('SELECT * FROM users WHERE u_name = $1', param );
+      return rows[0];
+    }finally{
+      await client.release();
+    }
   }
   static async insert( parameter ){
-    const { rows } = await pool.query(' INSERT INTO users ( u_name, u_email )VALUES( $1, $2 ) ', parameter )
-    return JSON.stringify(rows); 
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query(' INSERT INTO users ( u_name, u_email )VALUES( $1, $2 ) ', parameter )
+      return JSON.stringify(rows); 
+    }finally{
+      await client.release();
+    }
   }
   static async update( parameter ){
-    const { rows } = await pool.query(' UPDATE users set u_email = $2 WHERE u_name =  $1 ', parameter )
-    return JSON.stringify(rows); 
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query(' UPDATE users set u_email = $2 WHERE u_name =  $1 ', parameter )
+      return JSON.stringify(rows); 
+    }finally{
+      await client.release();
+    }
   }
   static async remove( parameter ){
-    const { rows } = await pool.query(' DELETE FROM users WHERE u_name = $1 ', parameter );
-    return JSON.stringify(rows);
+    let client = await pool.connect(); 
+    try{
+      const { rows } = await client.query(' DELETE FROM users WHERE u_name = $1 ', parameter );
+      return JSON.stringify(rows);
+    }finally{
+      await client.release();
+    }
   }
   static async end(){
-    pool.end();
+    console.log("END::Total Count: "+pool.totalCount);
+    console.log("END:: Idle Count: "+pool.idleCount);
+    //pool.end() ;
   }
   static testDB(){
-    //const oneUser = { 'u_name': 'test01', 'u_email': 'test01@u.com' };
     const oneUser = [ 'test01', 'test01@u.com' ];
     (async () => {
-    
-       console.log( "DB::test::insert::::" + JSON.stringify( DB.insert(oneUser) ) );
-       oneUser.u_email = 'test_01@u.com'; 
-       console.log( "DB::test::update::::" + JSON.stringify( DB.update(oneUser) ) ); 
-       console.log( "DB::test::remove::::" + JSON.stringify( DB.remove( [ oneUser[0] ] ) ) );
+       console.log( "DB::test::insert    ::::" + JSON.stringify( await DB.insert(oneUser) ) );
+       console.log( "DB::test::viewByName::::" + JSON.stringify( await DB.viewByName( [ oneUser[0] ]) ) );
+       oneUser[1] = 'test____01@u.com'; 
+       console.log( "DB::test::update    ::::" + JSON.stringify( await DB.update(oneUser) ) ); 
+       console.log( "DB::test::viewByName::::" + JSON.stringify( await DB.viewByName( [ oneUser[0] ]) ) );
+       console.log( "DB::test::remove    ::::" + JSON.stringify( await DB.remove( [ oneUser[0] ] ) ) );
+       console.log( "DB::test::viewByName::::" + JSON.stringify( await DB.viewByName( [ oneUser[0] ]) ) );
     })(); 
   }
 
